6417161144d5644a150cb34828b39ec544ee1126
francisco miranda <jaramira2001@gmail.com>
2026-08-16 12:22:37 -0700

    correccion de vulnerabilidades en dependencias

---
+++ app/(protected)/usuarios/page.tsx
@@ -0,0 +1,11 @@
+import ContenedorPagina from "@/components/ContenedorPagina";
+
+export default async function UsuariosPage(){
+    return(
+        <ContenedorPagina
+            titulo="Usuarios"
+        >
+            hola
+        </ContenedorPagina>
+    );
+}
